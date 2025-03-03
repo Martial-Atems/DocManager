a201ae59e77923e3f85443902791da44c5d97932
Martial <martialatems128@gmail.com>
2025-03-03 11:42:55 +0100

    mis a jour

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mysql from 'mysql2';
 
 //connection a la bdd
@@ -11,17 +10,3 @@ const connection = mysql.createPool({
 });
 
 export default connection;
-=======
-import mysql from 'mysql2';
-
-//connection a la bdd
-const connection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    port: 3306,
-    database: "projet_bd",
-});
-
-export default connection;
->>>>>>> 542e4fef0fe630f66727a9a546df5c4056eb763a
--- javaScript/utilisateurs.js
+++ javaScript/utilisateurs.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     const content = document.getElementById('content');
@@ -29,36 +28,4 @@ function toggleSidebar() {
 // afficherHeureEtDate();
 
 // // Mettre a jour l'heure et la date toutes les secondes
-=======
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    const content = document.getElementById('content');
-    sidebar.classList.toggle('collapsed');
-    content.classList.toggle('collapsed');
-}
-
-// // Afficharge de l'heure et de la date automatique
-// const heureElement = document.getElementById('heure');
-// const dateElement = document.getElementById('date');
-
-// function afficherHeureEtDate() {
-//     const dateActuelle = new Date();
-//     const heure = dateActuelle.getHours();
-//     const minute = dateActuelle.getMinutes();
-//     const seconde = dateActuelle.getSeconds();
-//     const jour = dateActuelle.getDate();
-//     const mois =dateActuelle.getMonth() + 1;
-//     const annee = dateActuelle.getFullYear();
-//     const Heures = `${jour}/${mois}/${annee}`;
-//     const Dates = `${heure}:${minute}:${seconde}`;
-
-//     // affichage
-//     heureElement.textContent = Heures;
-//     dateElement.textContent = Dates;
-// }
-
-// afficherHeureEtDate();
-
-// // Mettre a jour l'heure et la date toutes les secondes
->>>>>>> 542e4fef0fe630f66727a9a546df5c4056eb763a
 // setInterval(afficherHeureEtDate, 1000);
